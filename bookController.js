74bf11db752f0b778245d1593a341e3994942f0f
Kapil Verma <sparkskapil@gmail.com>
2019-10-08 18:42:24 +0530

    Handled no query string case

--- bookController.js
+++ bookController.js
@@ -1,13 +1,20 @@
 const utils = require('./utils');
 
 async function getEbookList(req, res) {
+  if (!req.query || !req.query.req) {
+    res.status(200);
+    return res.json({ err: 'Query string required!' });
+  }
   const ebooks = await utils.getEbooks(req.query);
   res.status(200);
   return res.json(ebooks);
 }
 
 async function getDownloadLink(req, res) {
-  console.log(req.query);
+  if (!req.query || !req.query.req) {
+    res.status(200);
+    return res.json({ err: 'Query string required!' });
+  }
   const book = await utils.getDownloadLink(req.query);
   res.status(200);
   return res.json({ book });
